b89ada7543ce83f983cf1fc1f49846be0da6d626
UbongPr7 <ubongpr7@gmail.com>
2025-08-05 20:26:46 +0100

    mcp tools setup

--- mainapps/inventory/mcp_views.py
+++ mainapps/inventory/mcp_views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Inventory
 from .api.serializers import InventorySerializer
-from rest_framework.exceptions import ValidationError
 
 class InventoryListAPIView(generics.ListAPIView):
     """
