3ededbb0ad93cbfc5cbb4ff42175c2acf669f13d
UbongPr7 <ubongpr7@gmail.com>
2025-08-05 20:21:35 +0100

    mcp tools setup

--- mainapps/company/mcp_views.py
+++ mainapps/company/mcp_views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Company, Branch, Department
+from .models import Company
 from .api.serializers import CompanySerializer
 from rest_framework.exceptions import ValidationError
 
