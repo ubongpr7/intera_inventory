bec3e1ad75dc0cd06c17eb8dc87abb9fad0544e0
UbongPr7 <ubongpr7@gmail.com>
2025-08-30 22:23:27 +0100

    analytics

--- core/settings.py
+++ core/settings.py
@@ -136,6 +136,27 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+# S3 Configuration
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CONNECT_TIMEOUT = 10  
+AWS_S3_TIMEOUT = 60 
+AWS_S3_FILE_OVERWRITE = True
+
+
+
+
+STORAGES = {
+        "default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
+        "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
