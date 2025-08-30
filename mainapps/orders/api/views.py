b70e1d3f2b72e9b600f6b69e1140294e23591c77
UbongPr7 <ubongpr7@gmail.com>
2025-08-30 20:22:01 +0100

    analytics

--- mainapps/orders/api/views.py
+++ mainapps/orders/api/views.py
@@ -11,7 +11,7 @@ from decimal import Decimal
 import logging
 
 from mainapps.inventory.models import InventoryTransaction, TransactionType
-from mainapps.orders.api.serializers import PurchaseOrderDetailSerializer, PurchaseOrderLineItemSerializer,PurchaseOrderLineItemCreateSerializer, PurchaseOrderListSerializer,ReceiveItemsSerializer
+from mainapps.orders.api.serializers import PurchaseOrderDetailSerializer,PurchaseOrderAnalyticsSerializer, PurchaseOrderLineItemSerializer,PurchaseOrderLineItemCreateSerializer, PurchaseOrderListSerializer,ReceiveItemsSerializer
 from mainapps.stock.models import StockItem
 from subapps.permissions.constants import PURCHASE_ORDER_PERMISSIONS, UNIFIED_PERMISSION_DICT
 from subapps.permissions.microservice_permissions import BaseCachePermissionViewset, HasModelRequestPermission, PermissionRequiredMixin
